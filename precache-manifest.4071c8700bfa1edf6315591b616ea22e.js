64d83b0afa2407e5132b9006c8cf1125ab414191
mahmoud khdre <34020725+mahmoudkhdre@users.noreply.github.com>
2019-08-19 13:27:29 +0200

    Updates

---
+++ precache-manifest.4071c8700bfa1edf6315591b616ea22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1375f08855dfc6e78abc9568f3f783",
+    "url": "/mahmoudkhdre/reactpro/index.html"
+  },
+  {
+    "revision": "49affd31429fac61624e",
+    "url": "/mahmoudkhdre/reactpro/static/css/main.265bfe42.chunk.css"
+  },
+  {
+    "revision": "f2f283532be3866869b6",
+    "url": "/mahmoudkhdre/reactpro/static/js/2.9ded89e5.chunk.js"
+  },
+  {
+    "revision": "49affd31429fac61624e",
+    "url": "/mahmoudkhdre/reactpro/static/js/main.7d4fecc6.chunk.js"
+  },
+  {
+    "revision": "ce2e4350a389e448790b",
+    "url": "/mahmoudkhdre/reactpro/static/js/runtime~main.d6765739.js"
+  }
+]);
